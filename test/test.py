10d81546c6d11f1417abb880e8eec5dab7ed99d2
”Anton <limonovtosha3228@gmail.com>
2024-04-29 11:51:14 +0300

    Update repr

--- test/test.py
+++ test/test.py
@@ -1,37 +1,28 @@
-def my_sort(A, l, r):
-   if l < r:
-      c = int((l+r)/2)
-      lef = my_sort(A, l, c)
-      rig = my_sort(A, c+1, r)
-      merege(A, l, r, c)
-   else:
-      return list[l]
-L = [5, 2, 4, 6, 1, 3, 2, 6]
-print(my_sort(L, 0, len(L)-1)) 
+def merge(left, right): # Функция слияния двух частей
+    res = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right): # Здесь мы начинаем заполнять новый массив по возрастанию элементами двух начальных массивов 
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+# Если какая-то часть дошла досрочно полность вошла в новый массив, остаток будет заполнен концом другой части (она отсортрованаи наибольшая)
+    if len(res)<len(left)+len(right):
+        while i < len(left):
+            res.append(left[i])
+            i += 1
+        while j < len(right):
+            res.append(right[j])
+            j += 1
+        return res
 
-def merege(A, l, r, c):
-   res = []
-   i, j = l, c+1
-   while i<c+1 and j<r+1:
-      if A[i]<A[j]:
-         res.append(A[i])
-         print('i=', i, '->', c)
-         i += 1
-      elif A[j]<A[i]:
-         res.append(A[j])
-         print('j=', j, '->', r)
-         j += 1
-
-   if len(res) != len(A):
-      while i<c+1:
-         res.append(A[i])
-         i+=1
-      while j<r+1:
-         res.append(A[j])
-         j+=1
-   return res
-A = [1, 3, 9, 2, 5, 10]
-l = 0
-r = len(A) - 1
-c = 2 
-print(merege(A, l, r, c))
+def my_sort(A, p, r): # Здесь реализован сам алгоиртм сортировки слиянием
+    if len(A) < 2:
+        return A
+    else:
+        c = int((p+r) / 2)
+        left = my_sort(A[:c], p, c)
+        right = my_sort(A[c:], c+1, r)
+        return merge(left, right)
